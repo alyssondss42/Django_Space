d2b071a071d3b8df19cebdfc2f59cb95876781f0
alyssondss42 <alyssondssoares@gmail.com>
2023-08-20 15:09:25 -0300

    Add cadastro e login de usuarios via interface

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 
 
 def login(request):
     form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+
+            usuario = auth.authenticate(request, username=nome, password=senha)
+
+            if usuario is not None:
+                auth.login(request, usuario)
+                return redirect('index')
+            else:
+                return redirect('login')
+
     return render(request, 'usuarios/login.html', {"form": form})
 
 
 def cadastro(request):
     form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            if form['senha_1'].value() != form['senha_2'].value():
+                return redirect('cadastro')
+
+            nome = form['nome_cadastro'].value()
+            email = form['email'].value()
+            senha = form['senha_1'].value()
+
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+
+            usuario = User.objects.create_user(username=nome,
+                                               password=senha,
+                                               email=email)
+            usuario.save()
+
+            return redirect('login')
+
+
+
     return render(request, 'usuarios/cadastro.html', {"form": form})
