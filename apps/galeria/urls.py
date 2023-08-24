cfa8c4feeb05141a4d23c2aa8b25708ddefd7a62
alyssondss42 <alyssondssoares@gmail.com>
2023-08-23 22:46:14 -0300

    Edicao e delecao de imagens

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path('buscar', buscar, name='buscar'),
-    path('nova-image', nova_imagem, name='nova_imagem'),
-    path('editar-image', editar_imagem, name='editar_imagem'),
-    path('deletar-image', deletar_imagem, name='deletar_imagem'),
+    path('nova_imagem', nova_imagem, name='nova_imagem'),
+    path('editar_imagem/<int:foto_id>', editar_imagem, name='editar_imagem'),
+    path('deletar_imagem/<int:foto_id>', deletar_imagem, name='deletar_imagem'),
 ]
