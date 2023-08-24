386d5ffa0bf881935304988d0c576bd762b4d9e2
alyssondss42 <alyssondssoares@gmail.com>
2023-08-23 21:48:23 -0300

    Add pagina de nova_imagem e ajuste do banco

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 
@@ -30,3 +31,14 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, 'galeria/buscar.html', {'cards': fotografias})
+
+
+def nova_imagem(request):
+    form = FotografiaForms
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
