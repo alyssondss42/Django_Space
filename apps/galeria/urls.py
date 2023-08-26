ae7f75137f5ee74da26e41739df944e5fca5afe1
alyssondss42 <alyssondssoares@gmail.com>
2023-08-26 20:01:54 -0300

    Add botoes de filtro na pagina de index

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.galeria.views import index, imagem, buscar, \
-    nova_imagem, editar_imagem, deletar_imagem
+    nova_imagem, editar_imagem, deletar_imagem, filtro
 
 urlpatterns = [
     path('', index, name='index'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('nova_imagem', nova_imagem, name='nova_imagem'),
     path('editar_imagem/<int:foto_id>', editar_imagem, name='editar_imagem'),
     path('deletar_imagem/<int:foto_id>', deletar_imagem, name='deletar_imagem'),
+    path('filtro/<str:categoria>', filtro, name='filtro'),
 ]
